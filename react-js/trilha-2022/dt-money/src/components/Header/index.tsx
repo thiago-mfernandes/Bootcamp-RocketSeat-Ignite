27cdbedee2b09542729ff88e692e66828d67f577
Thiago Fernandes <falecomobranco@yahoo.com.br>
2022-11-11 11:52:39 -0300

    inicizalizacao do projeto e primeiros componentes

---
+++ react-js/trilha-2022/dt-money/src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import Logo from "../../assets/logo.svg";
+
+export function Header() {
+  return(
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={Logo} alt="" />
+        <NewTransactionButton>Nova Transação</NewTransactionButton>
+      </HeaderContent>
+    </HeaderContainer>
+  )
+} 
